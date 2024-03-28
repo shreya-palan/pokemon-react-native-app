26422391b55f65c67b32e6d929e67e26dd7bb0e3
shreya <shreyab.udupi@gmail.com>
2024-03-28 15:41:02 +0530

    react native app done

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Platform, ScrollView } from 'react-native';
+import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+
+const charmanderData = {
+  name: "Charmander",
+  image: require("./assets/charmander.png"),
+  type: "Fire",
+  hp: 39,
+  moves: ["Scratch", "Ember", "Growl", "Leer"],
+  weakness: ["Water", "Rock"],
+};
+const bulbasaurData = {
+  name: "Bulbasaur",
+  image: require("./assets/bulbasaur.png"),
+  type: "Grass",
+  hp: 45,
+  moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+  weakness: ["Fire", "Ice", "Flying", "Psychic"],
+};
+const squirtleData = {
+  name: "Squirtle",
+  image: require("./assets/squiritlo.png"),
+  type: "Water",
+  hp: 44,
+  moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+  weakness: ["Electric", "Grass"],
+};
+const pikachuData = {
+  name: "Pikachu",
+  image: require("./assets/pikachu.jpg"),
+  type: "Electric",
+  hp: 35,
+  moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+  weakness: ["Ground"],
+};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <PokemonCard {...charmanderData}/>
+      <PokemonCard {...squirtleData}/>
+      <PokemonCard {...bulbasaurData}/>
+      <PokemonCard {...pikachuData}/>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
 });
